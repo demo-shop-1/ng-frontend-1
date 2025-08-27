016a38b29afe18fe2e7ad1f1192e73f4e099ea5c
Willy Arrojas <willyarrojas@gmail.com>
2025-08-27 13:29:45 +0200

    test theme and components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSlideToggleModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
